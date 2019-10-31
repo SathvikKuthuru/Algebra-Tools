a1b306a7a7c95bc48edc915cda173ad75de165bc
Sathvik Kuthuru <31018656+SathvikKuthuru@users.noreply.github.com>
2019-10-30 20:41:47 -0400

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,12 @@
 import utils.Expression;
+import utils.ExpressionValidator;
 
 public class Test {
     public static void main(String[] args) {
-        Expression expression = new Expression("x+5");
+        Expression expression = new Expression("(x+5)(4+2))");
+        ExpressionValidator expressionValidator = new ExpressionValidator(expression);
         System.out.println(expression.getUsedExpression());
         System.out.println(expression.getVariable());
+        System.out.println(expressionValidator.validate().getErrorMessage());
     }
 }
