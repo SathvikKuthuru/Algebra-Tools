dd2c5b495c672094c0d2aa84c384ce2822a9659b
Sathvik Kuthuru <31018656+SathvikKuthuru@users.noreply.github.com>
2019-10-30 21:12:12 -0400

    Update Term.java

--- src/utils/expressionsyntax/Term.java
+++ src/utils/expressionsyntax/Term.java
@@ -1,5 +1,13 @@
 package utils.expressionsyntax;
 
 public class Term implements Syntax {
+    int coefficient;
+    int exponent;
+    Character variable;
 
+    public Term(int c, int exp, Character v) {
+        this.coefficient = c;
+        this.exponent = exp;
+        this.variable = v;
+    }
 }
